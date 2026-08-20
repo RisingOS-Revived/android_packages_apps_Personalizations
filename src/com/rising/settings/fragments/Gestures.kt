0acc1c7eb32ed849223a424915a28d90a7203712
Arman-ATI <altafiarman5@gmail.com>
2026-08-21 00:31:31 +0330

    Update some Settings

--- src/com/rising/settings/fragments/Gestures.kt
+++ src/com/rising/settings/fragments/Gestures.kt
@@ -17,15 +17,26 @@ package com.rising.settings.fragments
 
 import android.content.Context
 import android.os.Bundle
+import android.os.UserHandle
+
+import androidx.preference.ListPreference
+import androidx.preference.Preference
+
 import com.android.internal.logging.nano.MetricsProto
 import com.android.settings.R
 import com.android.settings.search.BaseSearchIndexProvider
 import com.android.settingslib.search.SearchIndexable
 
+import lineageos.providers.LineageSettings
+
+import org.lineageos.internal.util.DeviceKeysConstants.Action
+
 @SearchIndexable
-class Gestures : OptimizedSettingsFragment() {
+class Gestures : OptimizedSettingsFragment(), Preference.OnPreferenceChangeListener {
 
     companion object {
+        private const val KEY_THREE_FINGERS_SWIPE = "three_fingers_swipe"
+
         /**
          * For search
          */
@@ -38,9 +49,50 @@ class Gestures : OptimizedSettingsFragment() {
         }
     }
 
+    private var mThreeFingersSwipeAction: ListPreference? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         addPreferencesFromResource(R.xml.rising_settings_gestures)
+
+        val threeFingersSwipeAction = Action.fromSettings(
+            contentResolver,
+            LineageSettings.System.KEY_THREE_FINGERS_SWIPE_ACTION,
+            Action.NOTHING
+        )
+        mThreeFingersSwipeAction = initList(KEY_THREE_FINGERS_SWIPE, threeFingersSwipeAction)
+    }
+
+    private fun initList(key: String, value: Action): ListPreference? {
+        return initList(key, value.ordinal)
+    }
+
+    private fun initList(key: String, value: Int): ListPreference? {
+        val list = preferenceScreen.findPreference<ListPreference>(key) ?: return null
+        list.value = value.toString()
+        list.summary = list.entry
+        list.onPreferenceChangeListener = this
+        return list
+    }
+
+    private fun handleListChange(pref: ListPreference, newValue: Any?, setting: String) {
+        val value = newValue as String
+        val index = pref.findIndexOfValue(value)
+        pref.summary = pref.entries[index]
+        LineageSettings.System.putIntForUser(
+            contentResolver, setting, Integer.valueOf(value), UserHandle.USER_CURRENT
+        )
+    }
+
+    override fun onPreferenceChange(preference: Preference, newValue: Any?): Boolean {
+        if (preference === mThreeFingersSwipeAction) {
+            handleListChange(
+                preference as ListPreference, newValue,
+                LineageSettings.System.KEY_THREE_FINGERS_SWIPE_ACTION
+            )
+            return true
+        }
+        return false
     }
 
     override fun getMetricsCategory(): Int {
